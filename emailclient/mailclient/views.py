4428d51a1c1aa7703ceaa05b2618cc9fce383e0c
Matvey-Makaro <matvey.makaro@gmail.com>
2022-06-03 11:59:37 +0300

    Add some basic views and urls.

--- emailclient/mailclient/views.py
+++ emailclient/mailclient/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Страница приложения почтового клиента.")
+
+
+def mails(request, mailid):
+    if request.GET:
+        print(request.GET)
+    return HttpResponse(f"<h1>Письмо номер</h1><p>{mailid}</p>")
+
+
+def archive(request, year): # Удалить потом, это просто для теста
+    if int(year) > 2022:
+        raise Http404()
+
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
