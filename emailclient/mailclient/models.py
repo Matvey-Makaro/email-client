362eb109d74e6233604504d2ae8d33930d4fb0a4
Matvey-Makaro <matvey.makaro@gmail.com>
2022-06-03 19:47:25 +0300

    Add User.

--- emailclient/mailclient/models.py
+++ emailclient/mailclient/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    login = models.CharField(max_length=255)
+    password = models.CharField(max_length=32)
+    registration_time = models.DateTimeField(auto_now_add=True)
+
+
