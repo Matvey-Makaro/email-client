0f1a74fe6090fee4f8d89f9cc54b898c5dcedf44
Matvey-Makaro <matvey.makaro@gmail.com>
2022-06-06 11:53:37 +0300

    Change SendEmailForm.

--- emailclient/mailclient/forms.py
+++ emailclient/mailclient/forms.py
@@ -2,8 +2,13 @@ from django import forms
 from .models import *
 
 
-class SendEmailFrom(forms.Form):
-    from_email = forms.EmailField()
+class SendEmailForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user')
+        if self.user is not None:
+            self.fields['from_email'].queryset= queryset=Email.objects.filter(user__pk=self.user.pk)
+
+    from_email = forms.ModelChoiceField(label="mailboxes", empty_label="select mailbox")
     to_email = forms.EmailField()
     subject = forms.CharField(max_length=255)
     message = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
