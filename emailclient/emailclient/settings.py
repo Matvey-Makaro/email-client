fd2492266a0838e7cc64cfb1001a7686305e0003
Matvey-Makaro <matvey.makaro@gmail.com>
2022-06-07 14:51:06 +0300

    Add css.

--- emailclient/emailclient/settings.py
+++ emailclient/emailclient/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -134,3 +136,5 @@ EMAIL_PORT = 465
 RECIPIENTS_EMAIL = ['matvey.makaro@gmail.com']   # замените на свою почту
 DEFAULT_FROM_EMAIL = 'matvey.makaro@gmail.com'  # замените на свою почту
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
